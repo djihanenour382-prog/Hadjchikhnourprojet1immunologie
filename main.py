cc262c0cc3ba8a088dcd1efee74f17021dfceb0d
djihanenour382-prog <djihanenour382@gmail.com>
2025-12-11 15:47:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#hadj chikh Nour djihane M1 Immunologie 13/12/2025
+#Les membres du groupe
+#Meniri chaima
+#Yagoub mohammed el amine 
+#arbi sihem
+#Arbi hayet
+
+import pandas as pd 
+#Donées:séquence ADN, Longueur ,Pourcentage
+data={
+"séquence":["ATGCGTACGTA","GCTAGCTAGGCC","ATGCGCGTAAGT","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
+"Longueur":[12,12,12,10,11,10,10],
+"Pourcentage GC":[50,66.67,58.33,40,45.45,60,50]
+}
+#Création d'un DataFrame(tableau pandas)
+df=pd.DataFrame(data)
+print("*************Création et Affichage **************","\n")
+print(df)
+#Affichage du tableau
+print("opération")
+
+
+
+
+
+
+
